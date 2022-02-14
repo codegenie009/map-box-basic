b9b8fe279e64f2f190d4f9dc419bef5057b74843
GS <ghostson1120.com>
2022-02-15 00:38:44 +0800

    map-box-basic

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Map from './_Map';
+
+function App() {
+  return (
+    <div>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/_Map.js
@@ -0,0 +1,46 @@
+import React, {useEffect, useRef, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import './Map.css';
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
+const Map  = () => {
+    const mapContainerRef = useRef(null);
+    
+    const [lng, setLng] = useState(5);
+    const [lat, setLat] = useState(34);
+    const [zoom, setZoom] = useState(1.5);
+    
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+
+        map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+        map.on('move', () => {
+            setLng(map.getCenter().lng.toFixed(4));
+            setLat(map.getCenter().lat.toFixed(4));
+            setZoom(map.getZoom().toFixed(2));
+        });
+
+        return () => map.remove();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    return (
+        <div>
+            <div className='sidebarStyle'>
+                <div>
+                    Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                </div>
+            </div>
+            <div className='map-container' ref={mapContainerRef} />
+        </div>
+    );
+};
+
+export default Map;
